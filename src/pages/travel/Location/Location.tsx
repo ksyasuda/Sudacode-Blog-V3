577e8ee8fa0903a9e0f5a070e62662f815cee347
ksyasuda <ksyasuda@umich.edu>
2021-01-21 16:58:02 -0500

    fix travel location component lat undefined error
    gatsby was complaining about latitude being undefined even though it
    isn't so I added a check to see if it is undefined before assigning and
    fixing the length

--- src/pages/travel/Location/Location.tsx
+++ src/pages/travel/Location/Location.tsx
@@ -19,12 +19,14 @@ const location: React.FC<LocationData> = ({
   time,
   key,
 }) => {
+  const latitude = lat ? lat.toFixed(2) : "ERROR: latitude undefined"
+  const longitude = lng ? lng.toFixed(2) : "ERROR: longitude undefined"
   return (
     <section key={key} className="location-item">
       <h2 className="location-title">{locName}</h2>
       <p className="date">{new Date(time).toLocaleString("en-US")}</p>
-      <p className="latitude">Latitude: {lat.toFixed(2)}</p>
-      <p className="longitude">Longitude: {lng.toFixed(2)}</p>
+      <p className="latitude">Latitude: {latitude}</p>
+      <p className="longitude">Longitude: {longitude}</p>
     </section>
   )
 }
